877f0d57ebde543b35b6b487eca11a24fdb93d9b
shupac800 <davidshuman@gmail.com>
2016-04-20 14:27:47 -0500

    added code to handle constants, regex to reject non-conforming constant names

--- simpleCalc/simpleCalc/Program.cs
+++ simpleCalc/simpleCalc/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace simpleCalc
@@ -15,12 +16,17 @@ namespace simpleCalc
             double result = 0;  // have to initialize double to a number
             bool runState = true;
             string last_inp = null;
+            Dictionary<string, double> constants = new Dictionary<string, double>();
 
             while(runState)
             {
                 string prompt = "[" + lineNum++.ToString() + "]> ";
                 Console.Write(prompt);
                 string inp = Console.ReadLine();
+
+                // strip out spaces
+                inp = Regex.Replace(inp," ","");
+
                 // was a command entered?
                 switch (inp)
                 {
@@ -41,13 +47,91 @@ namespace simpleCalc
                 }
                 last_inp = inp;
 
+                // are we defining a constant?
+                if (inp.IndexOf("=") > -1 )
+                {
+                    double n = 0;
+                    string[] defConstant = inp.Split('=');
+                    //Console.WriteLine("left side is {0}, right side is {1}",defConstant[0],defConstant[1]);
+                    // determine which side of the equality is a number
+                    bool rightSideIsNumeric = double.TryParse(defConstant[1], out n);
+                    bool leftSideIsNumeric = double.TryParse(defConstant[0], out n);
+                    if (leftSideIsNumeric && Regex.IsMatch(defConstant[1], @"^[a-z]+$"))
+                    {
+                        if (constants.ContainsKey(defConstant[1]))
+                        {
+                            Console.WriteLine("Error: constant {0} has already been defined\n", defConstant[1]);
+                        }
+                        else
+                        {
+                            constants.Add(defConstant[1], float.Parse(defConstant[0]));
+                            Console.WriteLine("defined {0} as {1}\n", defConstant[1], constants[defConstant[1]]);
+                            continue;
+                        }
+                    }
+                    else if (rightSideIsNumeric && Regex.IsMatch(defConstant[0], @"^[a-z]+$"))
+                    {
+                        if (constants.ContainsKey(defConstant[0]))
+                        {
+                            Console.WriteLine("Error: constant {0} has already been defined\n", defConstant[0]);
+                        }
+                        else
+                        {
+                            constants.Add(defConstant[0], float.Parse(defConstant[1]));
+                            Console.WriteLine("defined {0} as {1}\n", defConstant[0], constants[defConstant[0]]);
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Couldn't parse input\n");
+                        continue;
+                    }
+                }
+
                 for (int i = 0; i < operations.Length; i++)  // for each type of operator
                 {
                     if (inp.IndexOf(operations[i]) > -1)  // check to see if the input string contains this operator
                     {
                         string[] operands = inp.Split(operations[i][0]);   // first character of operations[i] is its only character
-                        double leftOperand = float.Parse(operands[0]);
-                        double rightOperand = float.Parse(operands[1]);
+                        double leftOperand = 0, rightOperand = 0;
+
+                        // check to see if we're using a constant
+                        if (Regex.IsMatch(operands[0], "[a-z]"))
+                        {
+                            if (constants.ContainsKey(operands[0]))
+                            {
+                                leftOperand = constants[operands[0]];
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error: {0} is not a defined constant\n", operands[0]);
+                                continue;
+                            }
+                        }
+                        if (Regex.IsMatch(operands[1], "[a-z]"))
+                        {
+                            if (constants.ContainsKey(operands[1]))
+                            {
+                                //leftOperand = float.Parse(operands[0]);
+                                rightOperand = constants[operands[1]];
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error: {0} is not a defined constant\n", operands[1]);
+                                continue;
+                            }
+                        }
+                        // regex expression from http://stackoverflow.com/questions/12117024/decimal-number-regular-expression-where-digit-after-decimal-is-optional
+                        if (Regex.IsMatch(operands[0], @"/^\d*\.?\d*$/"))
+                        {
+                            leftOperand = float.Parse(operands[0]);
+                        }
+                        if (Regex.IsMatch(operands[1], @"/^\d*\.?\d*$/"))
+                        {
+                            rightOperand = float.Parse(operands[1]);
+                        }
+
                         switch (operations[i])
                         {
                             case "+":
@@ -68,8 +152,8 @@ namespace simpleCalc
                             default:
                                 break;
                         }
-                        Console.WriteLine("   = " + result);
-                        i = operations.Length;  // no need to check for presence of other operators, so quit looping
+                        Console.WriteLine("   = " + result + "\n");
+                        continue;
                     }
                 }
             }
