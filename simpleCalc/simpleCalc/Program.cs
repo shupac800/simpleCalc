262ad2f719ca7f0dc939b8fd95008012e4dd6c60
shupac800 <davidshuman@gmail.com>
2016-04-20 12:20:46 -0500

    added command words

--- simpleCalc/simpleCalc/Program.cs
+++ simpleCalc/simpleCalc/Program.cs
@@ -12,11 +12,34 @@ namespace simpleCalc
         {
             string[] operations = new string[] { "+", "-", "*", "/", "%" };
             int lineNum = 0;
-            while(true)
+            double result = 0;  // have to initialize double to a number
+            bool runState = true;
+            string last_inp = null;
+
+            while(runState)
             {
                 string prompt = "[" + lineNum++.ToString() + "]> ";
                 Console.Write(prompt);
                 string inp = Console.ReadLine();
+                // was a command entered?
+                switch (inp)
+                {
+                    case "quit":
+                        runState = false;
+                        continue;
+                    case "exit":
+                        runState = false;
+                        continue;
+                    case "last":
+                        Console.WriteLine("   = " + result);
+                        continue;
+                    case "lastq":
+                        Console.WriteLine(last_inp);
+                        continue;
+                    default:
+                        break;
+                }
+                last_inp = inp;
 
                 for (int i = 0; i < operations.Length; i++)  // for each type of operator
                 {
@@ -25,7 +48,6 @@ namespace simpleCalc
                         string[] operands = inp.Split(operations[i][0]);   // first character of operations[i] is its only character
                         double leftOperand = float.Parse(operands[0]);
                         double rightOperand = float.Parse(operands[1]);
-                        double result = 0;  // have to initialize double to a number
                         switch (operations[i])
                         {
                             case "+":
@@ -46,7 +68,7 @@ namespace simpleCalc
                             default:
                                 break;
                         }
-                        Console.WriteLine(result);
+                        Console.WriteLine("   = " + result);
                         i = operations.Length;  // no need to check for presence of other operators, so quit looping
                     }
                 }
